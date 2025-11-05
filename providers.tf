d77051c01748b90f65f1f196d6608772a404e6d7
tithisaraswati <tithisaraswati720@gmail.com>
2025-11-05 06:10:02 +0000

    added s3 for storing terraform tf state file

--- providers.tf
+++ providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket         = "tithi-terraform-bucket"
+    key            = "dev/tithi/terraform.tfstate"
+    region         = "ap-southeast-2"
+    encrypt        = true
+    dynamodb_table = "tithi-roche-table1" # Optional, for DynamoDB locking
+      
+  }
   required_providers {
     aws = {
       source = "hashicorp/aws"
